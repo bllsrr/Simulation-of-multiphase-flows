13942bfc0ba75ec0113f83f3c37cf53979680557
bllsrr <bilalisarr@gmail.com>
2026-09-14 20:21:08 +0200

    Add files via upload

---
+++ air_in_water/method_1/turbulencedrops.c
@@ -0,0 +1,150 @@
+#include "grid/multigrid.h"
+#include "navier-stokes/centered.h"
+#include "two-phase.h"
+#include "tension.h"
+
+double tend = 5;
+int LEVEL = 7;
+double R0 = 0.1;
+double We;
+double Pe = 10;
+
+double rrhocp = 1.123 / 4172.4;
+double rkappa = 0.026 / 0.6;
+
+scalar fh1[]; //enthalpie fluid 1
+scalar fh2[]; //enthalpie fluid 2
+
+double rhocp1 = 1;
+double rhocp2;
+double kappa1;
+double kappa2;
+
+FILE * fpout;
+
+int main (int argc, char *argv[] ) {
+
+  X0 = Y0 = -0.5;
+  L0 = 1.;
+
+  size (L0);
+
+  rhocp2 = rhocp1/rrhocp;
+  kappa1 = 1/Pe;
+  kappa2 = kappa1/rkappa;
+
+  double Re = 100;
+  We = 5;
+  double rrho = 1.117 / 997.0;
+  double rmu = 0.0000185 / 0.001;
+
+  init_grid (1 << LEVEL);
+  
+  rho1 = 1.0;
+  rho2 = rho1/rrho;
+  mu1 = 1/Re;
+  mu2 = mu1/rmu;
+  f.sigma = 1/We;
+
+  /**
+  We reduce the tolerance on the Poisson and viscous solvers to
+  improve the accuracy. */
+  
+  TOLERANCE = 1e-4;
+
+  run();
+
+}
+
+scalar omega[];
+
+#include "initialcondition.h"
+event init (t = 0) {
+
+  fpout = fopen("output/data.vof", "w");
+
+  int Nvort = 10;
+
+  fraction (f, sq(R0) - (sq(y) + sq(x)) );
+
+  initial_condition(10, u);
+
+  foreach () {
+	fh1[] = rhocp1*f[];
+	fh2[] = 0.0;
+  }
+
+  f.tracers = list_copy ({fh1,fh2});
+  fh2.inverse = true;
+  
+}
+
+event diffusion (i++) {
+
+  scalar T[];
+  scalar s[];
+  scalar lambda[];
+  foreach () {
+	double rhocp = rhocp1*f[] + rhocp2*(1. - f[]);
+  	T[] = (fh1[] + fh2[])/rhocp;
+  	s[] = -rhocp*T[];
+	lambda[] = -rhocp;
+  }
+
+  face vector kappa[];
+  foreach_face () {
+	double ff = (f[1] + f[])/2.;
+	kappa.x[] = (kappa1*ff + kappa2*(1. - ff))*dt;
+  }
+
+  TOLERANCE = 1.e-4;
+  poisson(T, s, kappa, lambda);
+
+  //we reconstruct the enthalpy
+  foreach () {
+	fh1[] = rhocp1*T[]*f[];
+	fh2[] = rhocp2*T[]*(1.-f[]);
+  }
+
+}
+
+
+#if ADAPT
+event adapt (i++) {
+  adapt_wavelet ({f}, (double[]){1.e-2}, LEVEL);
+}
+#endif
+
+
+scalar ent[], ke[];
+event output (i++) {
+
+  scalar gfrac[], gw[], gw2[];
+  foreach () {
+    ent[] = fh2[];
+    ke[] = rho1*f[]*(sq(u.x[]) + sq(u.y[])) + rho2*(1. - f[])*(sq(u.x[]) + sq(u.y[]));
+  }
+
+
+  printf("%g %g %g \n", t, statsf(ent).sum, statsf(ke).sum);
+  fprintf(fpout, "%g %g %g \n", t, statsf(ent).sum, statsf(ke).sum);
+}
+
+
+
+FILE * fp;
+event video ( i +=10; t <= tend) {
+
+  vorticity (u, omega);
+  output_ppm (f, fp, n=2048, file= "output/f.mp4");
+  output_ppm (omega, fp, n=2048, file= "output/omega.mp4");
+
+  scalar T[];
+  foreach () {
+	double rhocp = rhocp1*f[] + rhocp2*(1. - f[]);
+  	T[] = (fh1[] + fh2[])/rhocp;
+  }
+  output_ppm (T, fp, n=2048, file= "output/T.mp4");
+
+}
+
---
+++ air_in_water/method_3/turbulencedrops.c
@@ -0,0 +1,133 @@
+#include "grid/multigrid.h"
+#include "navier-stokes/centered.h"
+#include "two-phase.h"
+#include "tension.h"
+#include "tracer.h"
+
+double tend = 5;
+int LEVEL = 7;
+double R0 = 0.1;
+double We;
+double Pe = 10;
+
+double rrhocp = 1.123 / 4172.4;
+double rkappa = 0.026 / 0.6;
+
+scalar xi[];
+scalar * tracers = {xi};
+
+double rhocp1 = 1;
+double rhocp2;
+double kappa1;
+double kappa2;
+
+FILE * fpout;
+
+int main (int argc, char *argv[] ) {
+
+  X0 = Y0 = -0.5;
+  L0 = 1.;
+
+  size (L0);
+
+  rhocp2 = rhocp1/rrhocp;
+  kappa1 = 1/Pe;
+  kappa2 = kappa1/rkappa;
+
+  double Re = 100;
+  We = 5;
+  double rrho = 1.117 / 997.0;
+  double rmu = 0.0000185 / 0.001;
+
+  init_grid (1 << LEVEL);
+  
+  rho1 = 1.0;
+  rho2 = rho1/rrho;
+  mu1 = 1/Re;
+  mu2 = mu1/rmu;
+  f.sigma = 1/We;
+
+  /**
+  We reduce the tolerance on the Poisson and viscous solvers to
+  improve the accuracy. */
+  
+  TOLERANCE = 1e-4;
+
+  run();
+
+}
+
+scalar omega[];
+
+#include "initialcondition.h"
+event init (t = 0) {
+
+  fpout = fopen("output/data.vof", "w");
+
+  int Nvort = 10;
+
+  fraction (f, sq(R0) - (sq(y) + sq(x)) );
+
+  initial_condition(10, u);
+
+  foreach () {
+	xi[] = 1.0*f[];
+  }
+  
+}
+
+event diffusion (i++) {
+
+  scalar s[];
+  scalar lambda[];
+  foreach () {
+	double rhocp = rhocp1*f[] + rhocp2*(1. - f[]);
+  	s[] = -rhocp*xi[];
+	lambda[] = -rhocp;
+  }
+
+  face vector kappa[];
+  foreach_face () {
+	double ff = (f[1] + f[])/2.;
+	kappa.x[] = (kappa1*ff + kappa2*(1. - ff))*dt;
+  }
+
+  TOLERANCE = 1.e-4;
+  poisson(xi, s, kappa, lambda);
+
+}
+
+
+#if ADAPT
+event adapt (i++) {
+  adapt_wavelet ({f}, (double[]){1.e-2}, LEVEL);
+}
+#endif
+
+
+scalar ent[], ke[];
+event output (i++) {
+
+  scalar gfrac[], gw[], gw2[];
+  foreach () {
+    ent[] = rhocp2*xi[]*(1-f[]);
+    ke[] = rho1*f[]*(sq(u.x[]) + sq(u.y[])) + rho2*(1. - f[])*(sq(u.x[]) + sq(u.y[]));
+  }
+
+
+  printf("%g %g %g \n", t, statsf(ent).sum, statsf(ke).sum);
+  fprintf(fpout, "%g %g %g \n", t, statsf(ent).sum, statsf(ke).sum);
+}
+
+
+
+FILE * fp;
+event video (i +=10; t <= tend) {
+
+  vorticity (u, omega);
+  output_ppm (f, fp, n=2048, min=0, max=1, file= "output/f.mp4");
+  output_ppm (omega, fp, n=2048, min=0, max=1, file= "output/omega.mp4");
+  output_ppm (xi, fp, n=2048, min=0, max=1, file= "output/T.mp4");
+
+}
+
